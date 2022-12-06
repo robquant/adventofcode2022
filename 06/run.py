34fc547f342a2074773acdc7d3f930422fb1698f
Robert Franke <robert.franke@nirx.de>
2022-12-06 11:57:43 +0100

    Add runtime measurement for day 6

--- 06/run.py
+++ 06/run.py
@@ -1,11 +1,20 @@
+import sys
+
 from more_itertools import windowed
+from tools import runtime
 
 def find_marker(text, length):
     for index, packet in enumerate(windowed(text, length)):
         if len(set(packet)) == length:
             return index + length
 
+@runtime
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    text = open("input").readline().rstrip('\n')
+    print("Part 1: ", find_marker(text, 4))
+    print("Part 2: ", find_marker(text, 14))
 
-text = open("input").readline().rstrip('\n')
-print("Part 1: ", find_marker(text, 4))
-print("Part 2: ", find_marker(text, 14))
+if __name__ == "__main__":
+    main()
